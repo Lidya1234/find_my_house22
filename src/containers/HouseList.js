8c44781f0a6d3fd761df6c20e6854e0a10683fc3
Lidya1234 <lidya94gher@gmail.com>
2021-05-20 20:14:55 +0300

    Provide the house component with the necessary info

--- src/containers/HouseList.js
+++ src/containers/HouseList.js
@@ -1,19 +1,29 @@
 import React, { useEffect }  from 'react';
 import  { useSelector, useDispatch } from 'react-redux';
 import { fetchHouses } from '../reducers/findSlice';
+// import CardColumns from 'react-bootstrap/CardColumns';
+import '../style.css';
 import House from '../components/House';
 const HouseList = () =>
 {  const dispatch =useDispatch()
-     const { house ,status } = useSelector(state => state.House)
+     const { house ,status } = useSelector(state => state.houses)
     useEffect(() =>
     {
      dispatch(fetchHouses())
     },[dispatch]);
-console.log(house , status)
+console.log(house , status ,'status')
     return(
-        <div>
-            <House />
+        <div className="wrapper">
+          
+                {
+                    house.map((home) =>
+                    <House key={home.id} image={home.image} name={home.name} price={home.price} rank={home.rank} />
+                    )
+                }
+ 
+    
         </div>
     )
 }
 export default HouseList;
+
