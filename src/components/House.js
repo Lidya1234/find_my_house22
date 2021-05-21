8b6d1c9a9197bfa62692aa3498101f5438540d60
Lidya1234 <lidya94gher@gmail.com>
2021-05-21 20:02:34 +0300

    Add rating stars

--- src/components/House.js
+++ src/components/House.js
@@ -1,12 +1,19 @@
-
-import '../style.css'
+import React from 'react';
+import '../style.css';
+import '../Rating/Rating.css';
 const House = ( { key, name, image, price, rank }) =>
-{
+{  const rate = (rank / 5) * 100
     return(
      <div className="house">
          <img className="image" src={image} alt={image} />
-         <div>
-             {name}{price}{rank}
+         <div className="description">
+           <div className="name">{name}
+           <div> <span className="star-wrapper">
+      <span className="stars" style={{ width: rate + "%" }}></span>
+    </span></div></div>
+             <div className="price">{price}
+             <div>per month</div></div>
+            
          </div>
      </div>
     )
