dddab461cd1f42ba99276467cee84eed7de304b3
Lidya1234 <lidya94gher@gmail.com>
2021-05-25 18:48:06 +0300

    Add page component

---
+++ src/components/Page.js
@@ -0,0 +1,11 @@
+import '../style/main.scss';
+import '../style/style.css'
+const Page = () =>
+{
+    return(
+        <div className="page">
+            <h4>1/64</h4>
+        </div>
+    )
+}
+export default Page;
