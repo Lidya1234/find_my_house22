813a39b975e2a65b69b1fd981b83e220d22d5137
Lidya1234 <lidya94gher@gmail.com>
2021-05-25 19:03:46 +0300

    Add collabsible text to SingleHouse

--- src/components/SingleHouse.js
+++ src/components/SingleHouse.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import '../style/style.css';
 import '../style/Rating.css';
+import ReactTextCollapse from 'react-text-collapse'
+import { FaAngleDown, FaAngleUp, FaCentercode } from 'react-icons/fa'
+
 const SingleHouse = ( { key, name, image, price, rank }) =>
-{  const rate = (rank / 5) * 100
+{  const rate = (rank / 5) * 100;
+
+    const TEXT_COLLAPSE_OPTIONS = {
+        collapse: false,
+        collapseText: <FaAngleDown />,
+        expandText: <FaAngleUp />,
+        minHeight: 50,
+        maxHeight: 100,
+        textStyle: {
+          color: 'gray',
+          fontSize: '20px',
+          float: 'center',
+          textAlign: 'center'
+        }
+      }
     return(
         <>
-     <div className="house">
-         <img className="image" src={image} alt={image} />
+     <div className="singlehouse">
+         <img className="singleimage" src={image} alt={image} />
          <div className="description">
            <div className="name">{name}
            <div> <span className="star-wrapper">
             <span className="stars" style={{ width: rate + "%" }}></span>
             </span></div></div>
-             <div className="price">{price}
+             <div className="price">$ {price}
              <div>per month</div></div>
             
          </div>
      </div>
-     <button type="button">Apply to rent</button>
+     <div className="collapsible">
+         <h6>About this listing</h6>
+     <ReactTextCollapse options={TEXT_COLLAPSE_OPTIONS}>
+         <p>gfkfngfmnhgjkvnbmnbfjkghfkdjghskdhfgughf
+             jbnvbvnbnmvbnjkghjfghfjg
+             hfjghfjghjfdghdskkhgfjdk</p>
+     </ReactTextCollapse>
+     </div>
+   
 </>
     )
 }
