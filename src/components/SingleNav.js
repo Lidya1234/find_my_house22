86d009b02f2ac0c919035742d847c85555a93b5a
Lidya1234 <lidya94gher@gmail.com>
2021-05-25 18:57:43 +0300

    Add style to SingleNav

--- src/components/SingleNav.js
+++ src/components/SingleNav.js
@@ -2,12 +2,13 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
 import { FaSearch,FaLessThan } from 'react-icons/fa'
 import '../style/main.scss'
-const SingleNav = ( { name }) => (
 
-<Navbar bg="light" variant="light" collapseOnSelect>
-<Navbar.Toggle aria-controls="basic-navbar-nav" />
-<Nav><FaLessThan /> </Nav>
-    <Navbar.Brand className="brand">{ name } </Navbar.Brand>
+const SingleNav = ( { name }) => (
+<Navbar bg="light" variant="light" expand="lg" collapseOnSelect>
+
+      <Nav.Link href="./"><FaLessThan /></Nav.Link>
+
+      <Navbar.Brand className="brand">{name}</Navbar.Brand>
    
   <div className="search">
     <input type="text" className="search__input" aria-label="search" placeholder="enter your search" />
@@ -17,4 +18,20 @@ const SingleNav = ( { name }) => (
   </Navbar>
 
 );
+
 export default SingleNav;
+
+// <Navbar bg="light" variant="light" collapseOnSelect>
+// <Navbar.Toggle aria-controls="basic-navbar-nav" />
+// <Nav className="mr-auto">
+// <Nav.Link href="#home"><FaLessThan /></Nav.Link>
+// </Nav>
+
+//     <Navbar.Brand className="singlebrand">{name}</Navbar.Brand>
+   
+//   <div className="search">
+//     <input type="text" className="search__input" aria-label="search" placeholder="enter your search" />
+//     <button className="search__submit" aria-label="submit search"><FaSearch /></button>
+//   </div>
+
+//   </Navbar>
