bbb0cc30151cf06254324c6c83aba363355758f5
Lidya1234 <lidya94gher@gmail.com>
2021-05-25 18:57:06 +0300

    Add link to home link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,12 +8,12 @@ const Navigation = () => (
 <Navbar.Toggle aria-controls="basic-navbar-nav" />
 <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="./">Home</Nav.Link>
       <Nav.Link href="#features">Features</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
     </Nav>
     </Navbar.Collapse>
-    <Navbar.Brand className="brand">House</Navbar.Brand>
+    <Navbar.Brand className="brand">Houses</Navbar.Brand>
    
   <div className="search">
     <input type="text" className="search__input" aria-label="search" placeholder="enter your search" />
