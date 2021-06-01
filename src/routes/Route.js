3a8948f474c5b808066c29d8fa7a7cfbac387839
Lidya1234 <lidya94gher@gmail.com>
2021-06-01 15:50:28 +0300

    Add loggen check in route.js

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,29 +1,69 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { fetchHouses } from '../reducers/findSlice';
+import { loginstatus } from '../reducers/findSlice';
 import HouseList from '../containers/HouseList';
 import SingleHouseList from '../containers/SingleHouseList';
 import Login from '../components/Login';
+import Logout from '../components/Logout';
 import '../style/main.scss';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Routes = () => {
   const dispatch = useDispatch();
-  const { house, status } = useSelector((state) => state.houses);
+  const { userInfo } = useSelector((state) => state.houses);
   useEffect(() => {
-    dispatch(fetchHouses());
+    dispatch(loginstatus());
   }, [dispatch]);
-  console.log(house, status);
+
   return (
     <BrowserRouter>
 
       <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/HouseList" component={HouseList} />
-        <Route exact path="/SingleHouseList" component={SingleHouseList} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            userInfo.logged_in ? (
+              <HouseList />
+            ) : (
+              <Login />
+            )
+          )}
+        />
+        <Route
+          exact
+          path="/HouseList"
+          render={() => (
+            userInfo.logged_in ? (
+              <HouseList />
+            )
+              : (
+                <div className="login-access">
+                  <p>You need to Sign In to access here</p>
+                </div>
+              )
+          )}
+        />
 
+        <Route
+          exact
+          path="/SingleHouseList"
+          render={() => (
+            userInfo.logged_in ? (
+              <SingleHouseList />
+            )
+              : (
+                <div className="login-access">
+                  <p>You need to Sign In to access here</p>
+                </div>
+              )
+          )}
+        />
+        <Route exact path="/Login" component={Login} />
+        <Route exact path="/Logout" component={Logout} />
+        {/* <div><h1>Access Denied.Please Login first</h1></div> */}
       </Switch>
 
     </BrowserRouter>
