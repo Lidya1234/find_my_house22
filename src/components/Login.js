d318fe3d1925bd201fb729e2a07f04a94f731328
Lidya1234 <lidya94gher@gmail.com>
2021-06-01 16:02:04 +0300

    Add redirect to singlehouselist using useHistory

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,66 +1,71 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { loginuser } from '../reducers/findSlice';
 import '../style/style.css';
+// import '../style/login.css';
 import '../style/main.scss';
 
-let username = '';
-let password = '';
-const dispatch = useDispatch();
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-  const user = {
-    username,
-    password,
+const Login = () => {
+  let username = '';
+  let password = '';
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const user = {
+      username,
+      password,
+    };
+    console.log(user);
+    dispatch(loginuser(user));
+    history.push('/HouseList');
   };
-  dispatch(loginuser(user));
-};
-const handleUNameChange = (e) => {
-  username = e.target.value;
-};
 
-const handlePasswordChange = (e) => {
-  password = e.target.value;
-};
-const Login = () => (
-  <div className="container">
-    <div className="login">
-      <h1>Sign in</h1>
-      <span>
-        Hello!There signin and
-        <div>start managing your system</div>
-      </span>
+  const handleUNameChange = (e) => {
+    username = e.target.value;
+    console.log(username);
+  };
+  const handlePasswordChange = (e) => {
+    password = e.target.value;
+    console.log(password);
+  };
+  return (
+    <div className="container">
+      <div className="login">
+        <h1>Sign in</h1>
+        <span>
+          Hello!There signin and
+          <div>start managing your system</div>
+        </span>
 
-      <form className="form">
-        <div>
-          <input
-            type="text"
-            placeholder="Enter username"
-            className="input"
-            name="username"
-            value={username}
-            onChange={handleUNameChange}
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            placeholder="Enter password"
-            className="input"
-            name="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <div>
-          <button type="submit" className="input btn-signin" onSubmit={handleSubmit}>
-            Sign In
-          </button>
-        </div>
-      </form>
-      <a href="#home" className="forgotpwd">Forgot password ?</a>
+        <form onSubmit={handleSubmit} className="form">
+          <div>
+            <input
+              type="text"
+              placeholder="Enter username"
+              className="txt-input"
+              name="username"
+              onChange={handleUNameChange}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Enter password"
+              className="txt-input"
+              name="password"
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div>
+            <button type="submit" className="txt-input btn-signin">
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default Login;
