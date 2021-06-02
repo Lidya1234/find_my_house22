abd48b8de8c7683eda7c8a38ae0b8bd696b7f6d1
Lidya1234 <lidya94gher@gmail.com>
2021-06-02 21:52:55 +0300

    Add test folder

---
+++ src/Test/Nav.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Nav from '../components/Nav';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('The navigation bar should contain', () => {
+  it('House logo', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>,
+    );
+    const link = getByText('Home');
+    expect(link).toBeInTheDocument();
+  });
+  const component = renderer.create(
+    <MemoryRouter>
+      <Nav />
+    </MemoryRouter>,
+  ).toJSON();
+
+  test('Matchs snapshot', () => {
+    expect(component).toMatchSnapshot();
+  });
+});
+
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
