f0aa21fe4062c55c86dca5ee7b2cecea1f4419e2
Lidya1234 <lidya94gher@gmail.com>
2021-06-08 18:33:36 +0300

    Remove the code loginstatus

--- src/reducers/findSlice.js
+++ src/reducers/findSlice.js
@@ -5,6 +5,7 @@ import { HTTP_STATUS } from '../constants/constants';
 export const fetchHouses = createAsyncThunk('house/fetchHouses',
   async () => {
     const { data } = await axios.get('https://findlidushouse.herokuapp.com/api/v1/houses');
+    console.log(data.data);
     return data.data;
   });
 
@@ -24,6 +25,7 @@ export const loginstatus = createAsyncThunk('status/loginstatus',
 export const loginuser = createAsyncThunk('user/loginuser',
   async (user) => {
     const { data } = await axios.post('https://findlidushouse.herokuapp.com/login', { user });
+    console.log(data);
     return data;
   });
 
@@ -35,7 +37,7 @@ export const logoutuser = createAsyncThunk('user/logoutuser',
 
 export const addfavorite = createAsyncThunk('favorite/addfavorite',
   async (favorite) => {
-    const { data } = await axios.post('https://findlidushouse.herokuapp.com/favorites', { favorite }, { withCredentials: true });
+    const { data } = await axios.post('https://findlidushouse.herokuapp.com/favorites', { favorite });
     return data.data;
   });
 export const fetchFavorite = createAsyncThunk('favorite/fetchFavorite',
@@ -52,12 +54,12 @@ export const findSlice = createSlice({
     favorite: [],
     status: 'idle',
     userInfo: {},
+    loggedin: false,
     user: {},
   },
   reducers: {
-    SETUSER: (state, action) => ({
-      filter: action.payload,
-      catalogues: state.catalogues,
+    CHANGE_LOGGEDIN: (state, action) => ({
+      loggedin: action.payload,
     }),
     CHANGE_SINGLE: (state, action) => ({
       single: action.payload,
@@ -97,32 +99,26 @@ export const findSlice = createSlice({
     [fetchFavorite.rejected](state) {
       state.status = HTTP_STATUS.REJECTED;
     },
-    [loginstatus.pending](state) {
+    [loginuser.pending](state) {
       state.status = HTTP_STATUS.PENDING;
     },
-    [loginstatus.fulfilled](state, action) {
+    [loginuser.fulfilled](state, action) {
       state.status = HTTP_STATUS.FULFILLED;
       state.userInfo = action.payload;
+      if (state.userInfo.logged_in) {
+        state.loggedin = true;
+      }
     },
-    [loginstatus.rejected](state) {
+    [loginuser.rejected](state) {
       state.status = HTTP_STATUS.REJECTED;
     },
-    // [loginuser.pending](state) {
-    //   state.status = HTTP_STATUS.PENDING;
-    // },
-    // [loginuser.fulfilled](state, action) {
-    //   state.status = HTTP_STATUS.FULFILLED;
-    //   state.userInfo = action.payload;
-    // },
-    // [loginuser.rejected](state) {
-    //   state.status = HTTP_STATUS.REJECTED;
-    // },
     [logoutuser.pending](state) {
       state.status = HTTP_STATUS.PENDING;
     },
     [logoutuser.fulfilled](state) {
       state.status = HTTP_STATUS.FULFILLED;
       state.userInfo = {};
+      state.loggedin = false;
     },
     [logoutuser.rejected](state) {
       state.status = HTTP_STATUS.REJECTED;
@@ -131,5 +127,5 @@ export const findSlice = createSlice({
 });
 /* eslint-enable no-param-reassign */
 // Action creators are generated for each case reducer function
-export const { SETUSER } = findSlice.actions;
+export const { CHANGE_LOGGEDIN } = findSlice.actions;
 export default findSlice.reducer;
