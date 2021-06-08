1085cdb4c6a98c97a82bddbff32e96459c626df4
Lidya1234 <lidya94gher@gmail.com>
2021-06-08 18:34:39 +0300

    Add code for persisting data on refresh

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import Route from './routes/Route';
 import store from './reducers/index';
 import reportWebVitals from './reportWebVitals';
 
+const persistor = persistStore(store);
 ReactDOM.render(
   <Provider store={store}>
-    <Route />
+    <PersistGate loading={null} persistor={persistor}>
+      <Route />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
