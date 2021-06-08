ef48360a34afe335d9638847592fbf1354a5dd36
Lidya1234 <lidya94gher@gmail.com>
2021-06-08 10:59:00 +0300

    Remove commented code

--- src/reducers/findSlice.js
+++ src/reducers/findSlice.js
@@ -5,47 +5,18 @@ import { HTTP_STATUS } from '../constants/constants';
 export const fetchHouses = createAsyncThunk('house/fetchHouses',
   async () => {
     const { data } = await axios.get('https://findlidushouse.herokuapp.com/api/v1/houses');
-    // , {
-    // mode: 'no-cors',
-    // withCredentials: true,
-    // credentials: 'include',
-    // headers: {
-    //   'Access-Control-Allow-Origin': '*',
-    //   'Content-Type': 'application/json',
-    //   'Access-Control-Allow-Credentials': true,
-    // },
-    // }
     return data.data;
   });
 
 export const fetchHouse = createAsyncThunk('house/fetchHouse',
   async (id) => {
     const { data } = await axios.get(`https://findlidushouse.herokuapp.com/api/v1/houses/${id}`);
-    // , {
-    //   mode: 'no-cors',
-    //   withCredentials: true,
-    //   credentials: 'include',
-    //   headers: {
-    //     'Access-Control-Allow-Origin': '*',
-    //     'Content-Type': 'application/json',
-    //     'Access-Control-Allow-Credentials': true,
-    //   },
-    // }
     return data.data;
   });
 
 export const loginstatus = createAsyncThunk('status/loginstatus',
   async () => {
     const { data } = await axios.get('https://findlidushouse.herokuapp.com/logged_in');
-    // {
-    //   mode: 'no-cors',
-    //   withCredentials: true,
-    //   credentials: 'include',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'Access-Control-Allow-Credentials': true,
-    //   },
-    // }
     console.log(data);
     return data;
   });
@@ -53,11 +24,6 @@ export const loginstatus = createAsyncThunk('status/loginstatus',
 export const loginuser = createAsyncThunk('user/loginuser',
   async (user) => {
     const { data } = await axios.post('https://findlidushouse.herokuapp.com/login', { user });
-    // , {
-    //   withCredentials: true,
-    //   mode: 'no-cors',
-    // }
-    // console.log(data);
     return data;
   });
 
@@ -141,16 +107,16 @@ export const findSlice = createSlice({
     [loginstatus.rejected](state) {
       state.status = HTTP_STATUS.REJECTED;
     },
-    [loginuser.pending](state) {
-      state.status = HTTP_STATUS.PENDING;
-    },
-    [loginuser.fulfilled](state, action) {
-      state.status = HTTP_STATUS.FULFILLED;
-      state.userInfo = action.payload;
-    },
-    [loginuser.rejected](state) {
-      state.status = HTTP_STATUS.REJECTED;
-    },
+    // [loginuser.pending](state) {
+    //   state.status = HTTP_STATUS.PENDING;
+    // },
+    // [loginuser.fulfilled](state, action) {
+    //   state.status = HTTP_STATUS.FULFILLED;
+    //   state.userInfo = action.payload;
+    // },
+    // [loginuser.rejected](state) {
+    //   state.status = HTTP_STATUS.REJECTED;
+    // },
     [logoutuser.pending](state) {
       state.status = HTTP_STATUS.PENDING;
     },
