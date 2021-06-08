d95fce31c7db09ad795d3f59ec40cc896f78fafe
Lidya1234 <lidya94gher@gmail.com>
2021-06-08 18:32:32 +0300

    Add code for persisting data on refresh

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import { combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import findReducer from './findSlice';
 
+const reducers = combineReducers({
+  houses: findReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export default configureStore({
-  reducer: {
-    houses: findReducer,
-  },
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
 });
