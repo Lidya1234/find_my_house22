077a6c107e45e22246fb294edaba1b10e1b8cae2
Lidya1234 <lidya94gher@gmail.com>
2021-06-18 12:53:05 +0300

    Remove the add and remove favourite button

--- src/containers/SingleHouseList.js
+++ src/containers/SingleHouseList.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useLocation } from 'react-router-dom';
-import { fetchHouse, addfavorite } from '../reducers/findSlice';
+import { fetchHouse } from '../reducers/findSlice';
 import SingleNav from '../components/SingleNav';
 import SingleHouse from '../components/SingleHouse';
 
@@ -13,30 +13,20 @@ const SingleHouseList = () => {
   useEffect(() => {
     dispatch(fetchHouse(id));
   }, [dispatch]);
-  const handleAddFavorite = (event) => {
-    event.preventDefault();
-    const userid = id;
-    const houseid = singlehouse.id;
-    const favorite = {
-      user_id: userid,
-      house_id: houseid,
-    };
-    dispatch(addfavorite(favorite));
-  };
+
   return (
     <>
       <SingleNav name={singlehouse.name} />
 
       <SingleHouse
         key={singlehouse.id}
+        id={singlehouse.id}
         image={singlehouse.image}
         name={singlehouse.name}
         price={singlehouse.price}
         rank={singlehouse.rank}
         description={singlehouse.description}
       />
-
-      <button type="button" className="appbtn" onClick={handleAddFavorite}>Add To Favorite</button>
     </>
   );
 };
