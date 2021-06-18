1435591da2b4c43848232946ffd69aabeb65fc56
Lidya1234 <lidya94gher@gmail.com>
2021-06-18 18:49:32 +0300

    Add test for login component

---
+++ src/Test/Login.test.js
@@ -0,0 +1,41 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import store from '../reducers/index';
+import Login from '../components/Login';
+
+afterEach(() => {
+  cleanup();
+});
+
+describe('House component tests', () => {
+  render(
+    <Provider store={store}>
+      <Router>
+        <Login />
+      </Router>
+    </Provider>,
+  );
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <Router>
+          <Login />
+        </Router>
+      </Provider>,
+    )
+    .toJSON();
+
+  const loginElement = screen.getByTestId('login');
+
+  test('Component should Render', () => {
+    expect(loginElement).toBeInTheDocument();
+  });
+  test('Component should contain a per month text', () => {
+    expect(loginElement).toHaveTextContent('Sign in');
+  });
+  test('Matches snapshot', () => {
+    expect(tree).toMatchSnapshot();
+  });
+});
